d1aabc0749645f0816a7fec6e177946ffd6e05a3
Cristian Lara <cristian89lara@gmail.com>
2025-05-09 19:50:12 -0500

    feat: ednpoint usuario agregado

---
+++ backend/src/infrastructure/controllers/userController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { UserService } from '../../application/services/userService';
+import { FirestoreUserRepository } from '../repositories/firestoreUserRepository';
+
+const userService = new UserService(new FirestoreUserRepository());
+
+export const userController = {
+  async getUserByEmail(req: Request, res: Response) {
+    const { email } = req.params;
+    const user = await userService.findByEmail(email);
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+    return res.json(user);
+  },
+
+  async createUser(req: Request, res: Response) {
+    const { email } = req.body;
+    let user = await userService.findByEmail(email);
+    if (user) {
+      return res.status(200).json(user);
+    }
+    user = await userService.create(email);
+    return res.status(201).json(user);
+  }
+}; 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import * as functions from 'firebase-functions';
 import { taskController } from './infrastructure/controllers/taskController';
 import { validateDto } from './infrastructure/middlewares/validationMiddleware';
 import { CreateTaskDto, UpdateTaskDto } from './domain/dtos/taskDto';
+import { userController } from './infrastructure/controllers/userController';
+import { CreateUserDto } from './domain/dtos/userDto';
 
 const app = express();
 
@@ -12,5 +14,7 @@ app.get('/tasks', taskController.getAllTasks);
 app.post('/tasks', validateDto(CreateTaskDto), taskController.createTask);
 app.put('/tasks/:id', validateDto(UpdateTaskDto), taskController.updateTask);
 app.delete('/tasks/:id', taskController.deleteTask);
+app.get('/users/:email', userController.getUserByEmail);
+app.post('/users', validateDto(CreateUserDto), userController.createUser);
 
 export const api = functions.https.onRequest(app);
