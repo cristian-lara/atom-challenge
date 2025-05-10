06c5b74c15f14ca13f8f9af5ce355c707e542f9f
Cristian Lara <cristian89lara@gmail.com>
2025-05-09 23:11:27 -0500

    feat: creado rutas y modulos con material

---
+++ frontend/src/app/tasks/tasks.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TasksRoutingModule } from './tasks-routing.module';
+
+@NgModule({
+  declarations: [TaskListComponent, TaskFormComponent],
+  imports: [CommonModule, RouterModule, TasksRoutingModule],
+  exports: [TaskListComponent, TaskFormComponent]
+})
+export class TasksModule {} 
