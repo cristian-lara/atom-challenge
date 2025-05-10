d69c556d726e682e92b55d75efc5568453df8384
Cristian Lara <cristian89lara@gmail.com>
2025-05-10 07:32:10 -0500

    feat: formulario de nueva tarea agregado y mostrado en la lista

--- frontend/src/app/tasks/task-list/task-list.component.ts
+++ frontend/src/app/tasks/task-list/task-list.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '@atom-challenge/shared';
+import { NotificationService } from '../../shared/notification/notification.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskFormComponent } from '../task-form/task-form.component';
 
 @Component({
   selector: 'app-task-list',
@@ -6,4 +11,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-list.component.scss'],
   standalone: false
 })
-export class TaskListComponent {} 
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
+  loading = true;
+
+  constructor(
+    private taskService: TaskService,
+    private notification: NotificationService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    this.taskService.getAll().subscribe({
+      next: (tasks) => {
+        this.tasks = tasks;
+        this.loading = false;
+      },
+      error: () => {
+        this.notification.error('Error al cargar las tareas');
+        this.loading = false;
+      }
+    });
+  }
+
+  trackByTaskId(index: number, task: Task) {
+    return task.id;
+  }
+
+  openAddTaskDialog() {
+    const dialogRef = this.dialog.open(TaskFormComponent, {
+      width: '100%',
+      maxWidth: '400px',
+      autoFocus: false,
+      data: null,
+      panelClass: 'task-dialog-panel'
+    });
+
+    dialogRef.componentInstance.save.subscribe((taskData: Partial<Task>) => {
+      dialogRef.close();
+      this.loading = true;
+      this.taskService.create(taskData).subscribe({
+        next: (task) => {
+          this.notification.success('Tarea creada correctamente');
+          this.tasks = [task, ...this.tasks];
+          this.loading = false;
+        },
+        error: () => {
+          this.notification.error('Error al crear la tarea');
+          this.loading = false;
+        }
+      });
+    });
+  }
+} 
--- frontend/src/app/tasks/tasks.module.ts
+++ frontend/src/app/tasks/tasks.module.ts
@@ -4,13 +4,31 @@ import { RouterModule } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TasksRoutingModule } from './tasks-routing.module';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TaskListComponent, TaskFormComponent],
   imports: [
     CommonModule,
     RouterModule,
-    TasksRoutingModule
+    TasksRoutingModule,
+    MatListModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
   ],
   exports: [TaskListComponent, TaskFormComponent]
 })
